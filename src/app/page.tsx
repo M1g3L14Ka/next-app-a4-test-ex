fe2b21b387e4ad6ea79be23fda689a6cb5592dfd
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2026-02-03 17:05:02 +0300

    next-app-test-ex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,5 @@ import PageWrapper from "./components/PageWrapper";
 export default async function Home() {
   const res = await fetch('https://t-core.fit-hub.pro/Test/GetTariffs', { cache: 'no-store' });
   const tariffs: ITariff[] = await res.json();
-
   return <PageWrapper tariffs={tariffs} />;
 }
